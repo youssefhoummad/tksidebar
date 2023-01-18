688805d784cb273795be6c04c59bc767ead4ad57
youssef hoummad <youssef.hoummad@outlook.com>
2023-01-18 18:16:12 +0100

    Update sidebar.py

--- src/tksidebar/sidebar.py
+++ src/tksidebar/sidebar.py
@@ -16,6 +16,18 @@ def get_accent_color():
   accent = accent[4:6]+accent[2:4]+accent[0:2]
   return '#'+accent
 
+
+def color_variant(hex_color, brightness_offset=10):
+  """ takes a color like #87c95f and produces a lighter or darker variant """
+  if len(hex_color) != 7:
+      raise Exception("Passed %s into color_variant(), needs to be in #87c95f format." % hex_color)
+  rgb_hex = [hex_color[x:x+2] for x in [1, 3, 5]]
+  new_rgb_int = [int(hex_value, 16) + brightness_offset for hex_value in rgb_hex]
+  new_rgb_int = [min([255, max([0, i])]) for i in new_rgb_int] # make sure new values are between 0 and 255
+  # hex() produces "0x88", we want just "88"
+  return "#" + "".join([hex(i)[2:] for i in new_rgb_int])
+
+
 default_colors = {"accent": get_accent_color(), "bg":'#EFF4F8',"hover":'#E8EBF0',"press":'#D6D9DE'}
 
 
@@ -54,6 +66,9 @@ class Sidebook(tk.Frame):
 
   
   def set_colors(self, colors):
+#     bg = colors.get('bg', '#EFF4F8')
+#     default_colors = {"accent": get_accent_color(), "bg":'#EFF4F8',"hover":color_variant(bg, 20),"press":color_variant(bg, -20)}
+    
     _colors = default_colors
     if colors:
       for key, value in colors.items():
